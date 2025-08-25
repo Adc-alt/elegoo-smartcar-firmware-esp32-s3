76d8bf7e8db00b99b8028894244ccacdca0d4ade
Antonio Delgado <antoniodelgadodc98@gmail.com>
2025-08-25 23:09:48 +0200

    Codigo 2: Curro issue

---
+++ lib/elegoo_smartcar_lib.h
@@ -0,0 +1,3 @@
+// UART2 pins (ajusta si hace falta)
+#define UART2_TX 40
+#define UART2_RX 3
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,40 +1,47 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include "../lib/elegoo_smartcar_lib.h"
 
-// UART2 pins (ajusta si hace falta)
-#define UART2_TX 40
-#define UART2_RX 3
 
 // Timeouts
 #define TIMEOUT_INTERVAL 250
+const unsigned long PERIOD_MS = 50; // 
 // Estado de recepción
 String buffer;
 bool   receiving     = false;
 unsigned long lastMessageTime   = 0;
+unsigned long lastSentTime      = 0;
 
 JsonDocument doc;
 
 
 void readMessage();
 void checkTimeout();
+void sendMessage(JsonDocument &doc, unsigned long interval);
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);                                
   Serial2.begin(9600, SERIAL_8N1, UART2_RX, UART2_TX); 
   Serial.println("ESP32 listo para recibir JSON por UART");
+  // Inicializar valores del JSON
+  doc["temp"] = 23.5;
+  doc["hum"]  = 60;
 }
 
 
 
 void loop() 
 {
-  readMessage();
-  checkTimeout();
+  sendMessage(doc,PERIOD_MS);
+  // readMessage();
+  // checkTimeout();
 }
 
 void readMessage() 
 {
-  while (Serial2.available()) {
+  while (Serial2.available()) 
+  {
     char c = Serial2.read();
 
     
@@ -69,4 +76,17 @@ void checkTimeout()
   }
 }
 
+
+void sendMessage(JsonDocument &doc, unsigned long interval)
+{
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastSentTime >= interval)  // ← Usar variable global
+  {
+    lastSentTime = currentTime;
+    serializeJson(doc, Serial2);
+    Serial2.println();
+  }
+  Serial.println("Mensaje enviado");  
+}
 
